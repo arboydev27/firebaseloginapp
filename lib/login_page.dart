5aa1b0d20896beb9992d3c54c8ba36ddcdfe7274
Arboy Magomba <arboydev@gmail.com>
2024-02-28 17:42:26 -0600

    Initial commit: Added login and logout functionaility

---
+++ lib/login_page.dart
@@ -0,0 +1,153 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, unused_field
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  // Text controllers
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  // Asynchronous;y check in if we can sign in or not
+  Future signIn() async {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: _emailController.text.trim(), 
+      password: _passwordController.text.trim());
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+            
+                Icon(
+                  Icons.android,
+                  size: 100,
+                ),
+            
+                SizedBox(height: 30),
+            
+                // Hello again!
+                Text(
+                  "Hello Again!",
+                  style: GoogleFonts.bebasNeue(
+                    fontSize: 52,
+                  ),
+                  ),
+            
+                  SizedBox(height: 10),
+            
+                Text("Welcome back, try to login!",
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+                ),
+            
+                SizedBox(height: 50),
+            
+                // EMail textfield
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: TextField(
+                    controller: _emailController,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.deepPurple),
+                        borderRadius: BorderRadius.circular(12)
+                      ),
+                      hintText: "Email",
+                      fillColor: Colors.grey[200],
+                      filled: true,
+                    ),
+                  ),
+                ),
+            
+                SizedBox(height: 10),
+            
+                // Password textfield
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: TextField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.deepPurple),
+                        borderRadius: BorderRadius.circular(12)
+                      ),
+                      hintText: "Password",
+                      fillColor: Colors.grey[200],
+                      filled: true,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+            
+                // Sign in button
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: GestureDetector(
+                    onTap: signIn,
+                    child: Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Center(
+                        child: Text("Sign in",
+                        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                        )),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 25),
+            
+                // Not a member space
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Not a member yet?", style: TextStyle(fontWeight: FontWeight.bold),),
+                    Text(" Register Now", style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),)
+                  ],
+                ),
+            
+              ],
+            ),
+          ),
+        ),
+      ),
+
+    );
+  }
+}
---
+++ lib/main_page.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebaseloginapp/home_page.dart';
+import 'package:firebaseloginapp/login_page.dart';
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return HomePage();
+        } 
+        else{
+          return LoginPage();
+        }
+      },
+      ),
+    );
+  }
+}
